61c487e3b2db37203eecbc88fd31f1a668597d6f
Nxo <adu_x1421@163.com>
2026-09-11 18:15:59 +0800

    feat: 添加general设置选项到json

--- src/shared/Types/Settings.ts
+++ src/shared/Types/Settings.ts
@@ -1,23 +1,4 @@
-export interface Shortcut {
-  name: string
-  path: string
-  importedFromFolder?: string // id to map it to a folder
-}
-
-export function isShortcutEqual(a: Shortcut, b: Shortcut) {
-  return a.name === b.name && a.path === b.path
-}
-
-export function serializeShortcut(shortcut: Shortcut): any {
-  return {
-    name: shortcut.name,
-    path: shortcut.path,
-    importedFromFolder: shortcut.importedFromFolder
-  }
-}
-
-
-
+import type { Shortcut } from "./Shortcut"
 /*
 Type:
 
